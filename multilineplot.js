4ec259839e06877bb0fcfb591f54eec22f428b41
Prasanth Prahladan <prashvirgoan@gmail.com>
2016-06-04 16:41:14 -0600

    include mlp.js file

--- multilineplot.js
+++ multilineplot.js
@@ -1,39 +1,27 @@
-function plotMultiLine(cellData) {
+function plotMultiLine(cellData, params) {
+
+var width = params.width, height = params.height, margin = params.margin;
 
 var cellIDs = [];
 for (var i = 0, len = cellData.length; i < len; i++) {
   cellIDs.push(cellData[i].cellID);
-};
+}
 
+// define color object to associate each trace with different color
 var color = d3.scale.category10().domain(cellIDs);
 
-var margin = {top: 20, right: 80, bottom: 30, left: 50},
-width = 960 - margin.left - margin.right,
-height = 500 - margin.top - margin.bottom;
-
-
+// define linear x-y scales for the plots/graphs.
 var xScale = d3.scale.linear().range([0, width]);
 var yScale = d3.scale.linear().range([height, 0]);
 
-var xAxis = d3.svg.axis()
-  .scale(xScale)
-  .orient("bottom");
+// =============================================================================
+// Define the html-svg elements and associate them with data.
 
-var yAxis = d3.svg.axis()
-  .scale(yScale)
-  .orient("left");
-
-  // create a line function that can convert data[] into x and y points
-  var linePlot = d3.svg.line()
-    .x(function(datum,indx) {  return xScale(indx); })
-    .y(function(datum) { return yScale(datum);})
-
-
-    var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
 // Add an SVG element with the desired dimensions and margin.
@@ -48,30 +36,35 @@ var graph = d3.select("#multilineplot").append("svg:svg")
       .attr("class","aLinePlot");
 
 
+//--------------------------------------------------------------------------
+// associate Data_set to the graph!
 graph.data(function(){
 
-  //--------------------------------------------------------------------------
   // update the Scales!
-
   xScale.domain([0, cellData[0].voltSig.length]);
   yScale.domain([
     d3.min(cellData,function(c){return d3.min(c.voltSig);}),
     d3.max(cellData,function(c){return d3.max(c.voltSig);})
   ]);
 
-//--------------------------------------------------------------------------
-   // associate Data_set to the graph!
+  // associate cellData to the graph-object.
   return cellData; // assosciate subset of cellData to the plot_graph.
 });
 
+// =============================================================================
+// Define functions to modify svg-elements.
+
 //--------------------------------------------------------------------------
-// set the axis
 // create a line function that can convert data[] into x and y points
 var linePlot = d3.svg.line()
   .x(function(datum,indx) {  return xScale(indx); })
-  .y(function(datum) { return yScale(datum);})
+  .y(function(datum) { return yScale(datum);});
 
-  // create yAxis
+  //--------------------------------------------------------------------------
+  // define axis objects
+
+
+  // create xAxis
   var xAxis = d3.svg.axis().scale(xScale).tickSize(-height).tickSubdivide(true);
   // Add the x-axis.
   graph.append("svg:g")
@@ -90,8 +83,8 @@ var linePlot = d3.svg.line()
 
 
 
-        //----------------------------------------------------------------------------
-        // plot the voltage signals
+//----------------------------------------------------------------------------
+// plot the voltage signals
 var sig = graph.selectAll(".sig")
       .data(cellData)
     .enter().append("g")
@@ -102,4 +95,4 @@ var sig = graph.selectAll(".sig")
       .attr("d", function(d) { return linePlot(d.voltSig); })
       .style("stroke", function(d) { return color(d.cellID); });
 
-};
+}
